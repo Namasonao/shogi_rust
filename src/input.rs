b58407f364b410d027712af8348ed875533a123e
Bas Niekel <bas@niekel.net>
2023-02-11 21:28:26 +0100

    fixed coordinates again to work for place

--- src/input.rs
+++ src/input.rs
@@ -54,16 +54,17 @@ fn parse_place(splits: Vec<&str>) -> Result<PlaceIdentifier, String> {
         return Err("Not enough inputs".to_string());
     }
 
-    let i: usize = (splits[1].as_bytes()[0] - 'a' as u8).into();
-    if i > 26 {
-        return Err("Invalid piece to place".to_string());
+    let c = splits[1].as_bytes()[0] as u8;
+    if c > 'z' as u8 || c < 'a' as u8 {
+        return Err("Invalid character".to_string());
     }
+    let i: usize = (c - 'a' as u8).into();
    let end_x = match splits[2].parse::<usize>() {
-       Ok(val) => val,
+       Ok(val) => from_coordinate_x(val)?,
        Err(err) => return Err(err.to_string()),
    };
    let end_y = match splits[3].parse::<usize>() {
-       Ok(val) => val,
+       Ok(val) => from_coordinate_y(val)?,
        Err(err) => return Err(err.to_string()),
    };
 
@@ -84,5 +85,5 @@ fn from_coordinate_y(p: usize) -> Result<usize, String> {
     if p > 9 || p < 1 {
         return Err("Out of bounds".to_string());
     }
-    return Ok(p + 1);
+    return Ok(p - 1);
 }
